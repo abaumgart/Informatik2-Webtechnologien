0b7a1146ab93f24d5d7de32ec4bcf299e47101b4
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2022-12-01 11:02:44 +0100

    Diagramm eingefügt

--- public_html/index.php
+++ public_html/index.php
@@ -3,8 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Startseite</title>
+	 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </head>
 <body>
+	
+	
 		<h1>Informatik 2 im Web</h1>
 		<p>Dies wird eine kleine Webanwendung</p>
 		<?php
@@ -18,11 +21,38 @@
 			include 'includes/dbconnect.inc.php';
 			
 		?>
+	<script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawVisualization);
+
+      function drawVisualization() {
+        // Some raw data (not necessarily accurate)
+        var data = google.visualization.arrayToDataTable([
+          ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
+          ['2004/05',  165,      938,         522,             998,           450,      614.6],
+          ['2005/06',  135,      1120,        599,             1268,          288,      682],
+          ['2006/07',  157,      1167,        587,             807,           397,      623],
+          ['2007/08',  139,      1110,        615,             968,           215,      609.4],
+          ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+        ]);
+
+        var options = {
+          title : 'Monthly Coffee Production by Country',
+          vAxis: {title: 'Cups'},
+          hAxis: {title: 'Month'},
+          seriesType: 'bars',
+          series: {5: {type: 'line'}}
+        };
+
+        var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
 			<form action="index.php" method="post">
 				<p><input name="vor">Vorname</p>
 				<p><input name="nach">Nachname</p>
 				<p><input name="gesendet" type="submit"></p>
 			</form>	
-		
+	<div id="chart_div" style="width: 900px; height: 500px;"></div>	
 </body>
 </html>
