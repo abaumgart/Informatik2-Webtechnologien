d6e52685b4c24431b4ee505e7964a3dad1935346
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2022-12-08 13:23:16 +0100

    Update

--- public_html/index.php
+++ public_html/index.php
@@ -16,8 +16,18 @@
 			   damit die Funktion vorhanden sind, 
 			   wenn sie benötigt werden. 
 			*/
+		if(isset($_POST["gesendet"]))
+			{
+				$selektionsdatum = $_POST["tagesauswahl"];
+				echo ("Selektiertes Datum: ".$selektionsdatum."<br>");
+			}else
+			{		
+				$selektionsdatum ="2022-06-01";
+
+			}
+	
 			include 'includes/functions.inc.php';
-			include 'includes/test.inc.php';
+			
 			include 'includes/dbconnect.inc.php';
 	
 		/* Daten auslesen für den Datumspicker */
@@ -36,21 +46,30 @@
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawVisualization);
 
+		
       function drawVisualization() {
         // Some raw data (not necessarily accurate)
         var data = google.visualization.arrayToDataTable([
-          ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
-          ['2004/05',  165,      938,         522,             998,           450,      614.6],
-          ['2005/06',  135,      1120,        599,             1268,          288,      682],
-          ['2006/07',  157,      1167,        587,             807,           397,      623],
-          ['2007/08',  139,      1110,        615,             968,           215,      609.4],
-          ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+          ['Zeit', '01.06.2022'],
+			<?php
+				$queryData = "select 
+								date_format(zeitstempel, \"%H\") as 'zeitstempel',
+								wert as 'Verbrauch' 
+								from energieverbrauch 
+								where zeitstempel between '2022-06-01 01:00:00' 
+								and '2022-06-01 23:00:00';";
+			$queryDataExec= mysqli_query($con,$queryData);
+			while($rowData=mysqli_fetch_array($queryDataExec))
+			{
+				echo "['".$rowData['zeitstempel']."',".$rowData['Verbrauch']."],";
+			}
+         ?>
         ]);
-
+			
         var options = {
-          title : 'Monthly Coffee Production by Country',
-          vAxis: {title: 'Cups'},
-          hAxis: {title: 'Month'},
+          title : 'Energieverbrauch',
+          vAxis: {title: 'Verbrauch'},
+          hAxis: {title: 'Uhrzeit'},
           seriesType: 'bars',
           series: {5: {type: 'line'}}
         };
